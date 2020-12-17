6360c7d90a3f3ecc08b00c896f31211dff86441f
giuliusONWARE <giuliuscotelo333@gmail.com>
2020-12-17 17:01:45 -0300

    onwaredev

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         
         <div>
-            <h1>Hello World 2</h1>
+            <h1>Hello World 3</h1>
             <h3>Site em desenvolvimento</h3>
             <p>By: Giulius</p>
 
